b3f245313b59ab9369ed532cb1c147b9f3eff0c1
SHAKUR <SHAKUR@SHAKUR-PC>
2018-10-16 12:35:36 +0100

    Initial Commit

---
+++ public_html/jascriptpr.js
@@ -0,0 +1,8 @@
+var text = "Welcome to binary computers nigrira limited";
+var slicedtext = text.toUpperCase(text);
+console.log(slicedtext);
+
+setInterval(function(){
+    var x = new Date();
+    console.log(x);
+},1000);
